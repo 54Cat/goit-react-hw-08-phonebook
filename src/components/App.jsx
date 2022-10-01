74a86ba2bf21244499bcb6e68abde00230967a60
Kateryna Hrabanova <KaterynaHrabanova@gmail.com>
2022-10-01 10:44:17 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,21 @@ export default class App extends React.Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   duplicateContact = (name) => {
     const { contacts } = this.state;
     const normalizedName = name.toLowerCase();
